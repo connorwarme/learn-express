dbd88bf4db47b442d78ab40c9f1a374bb4b584f7
connorwarme <connor.warme@gmail.com>
2023-01-26 16:01:59 -0700

    Intro to middleware

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const app = express()
 const wiki = require('./wiki')
 const port = 3000
 
+const myLogger = (req, res, next) => {
+    console.log(`req IP: ${req.ip}`)
+    console.log(`req method: ${req.method}`)
+    console.log(`req date: ${new Date()}`)
+
+    next()
+}
+
+app.use(myLogger)
+
 app.get("/", (req, res) => {
     res.send("Hello World! built w/ express")
 })
