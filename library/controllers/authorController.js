0a2cf665e5fb7c850168201f5bb3fb87edb74cd4
connorwarme <connor.warme@gmail.com>
2023-01-30 16:55:16 -0700

    Add to author controller for detail display

--- library/controllers/authorController.js
+++ library/controllers/authorController.js
@@ -1,4 +1,6 @@
 const Author = require("../models/author");
+const Book = require("../models/book");
+const async = require("async");
 
 // display list of all authors
 exports.author_list = (req, res, next) => {
@@ -16,8 +18,35 @@ exports.author_list = (req, res, next) => {
 }
 
 // display detail page for specific author
-exports.author_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Author Detail: ${req.params.id}`);
+exports.author_detail = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id)
+          .exec(callback)
+      },
+      books(callback) {
+        Book.find( { author: req.params.id }, "title summary")
+          .sort("book.title", "ascending")
+          .exec(callback);
+      }
+    }
+  ), 
+  (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    if (result.author == null) {
+      const err = new Error("Author not found!");
+      err.status = 404;
+      return next(err);
+    }
+    res.render("author_detail", { 
+      title: "Author Detail:", 
+      author: result.author, 
+      books: result.books 
+    })
+  }
 }
 
 // display author create form on GET
