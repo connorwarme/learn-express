9cab685041ce02f08ee8b4fc0e4658cf2f9c68dd
connorwarme <connor.warme@gmail.com>
2023-01-30 12:23:04 -0700

    Update bookinstance controller for yielding list

--- library/controllers/bookInstanceController.js
+++ library/controllers/bookInstanceController.js
@@ -1,10 +1,19 @@
 const BookInstance = require('../models/bookinstance');
 
 // display list of all book instances
-exports.bookinstance_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance list");
+exports.bookinstance_list = (req, res, next) => {
+  BookInstance.find()
+    .populate("book")
+    .exec((err, list_bookinstances) => {
+      if (err) {
+        return next(err);
+      }
+      res.render("bookinstance_list", {
+        title: "Book Instance List",
+        bookinstance_list: list_bookinstances,
+      })
+    })
 }
-
 
 // display detail page for a specific BookInstance.
 exports.bookinstance_detail = (req, res) => {
